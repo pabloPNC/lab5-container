fe23cff0913e71d4564b9431a1823ac6ed359dcf
pabloPNC <pablo.navarrocarpio@gmail.com>
2022-01-23 21:17:27 +0100

    Evaluación con Singularity

--- submit_eval_singularity.sh
+++ submit_eval_singularity.sh
@@ -4,23 +4,28 @@
 #SBATCH -p hpc-bio-ampere
 #SBATCH --chdir=/home/alumno18/lab5
 #SBATCH -J time-lab5
-#SBATCH --output=RESULTADOS.txt
+#SBATCH --output=RESULTADOS_sing.txt
 #SBATCH --exclusive
 
+# Cargar el modulo
+module load singularity
 
+imagen=/nas/hdd-0/singularity_images/python_3-9-1.sif
 
 echo "* Ejecutando k-mer13 C++ *"
 echo
-time ./k-mer13
+time singularity exec $imagen ./k-mer13
 echo "* Ejecutando k-mer14 C++ *"
 echo
-time ./k-mer14
+time singularity exec $imagen ./k-mer14
 
 echo "=========================="
 
 echo "* Ejecutando k-mer13 Python *"
 echo 
-time ./k-mer13.py
+time singularity exec $imagen ./k-mer13.py
 echo "* Ejecutando k-mer14 Python *"
 echo
-time ./k-mer14.py
+time singularity exec $imagen ./k-mer14.py
+
+module purge
